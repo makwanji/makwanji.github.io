15cff0971b9b44f2a20a66f570078b165073c0b3
Jignesh <makwana.jignesh@gmail.com>
2022-05-07 13:42:50 +0800

    Updates

---
+++ precache-manifest.ae6f728fe7f9b6e2b6b957b2b7949e22.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f705f4dd95baf3a3e595131c429c78a",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "815b3aaf55c7aaa6b330",
+    "url": "/home/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/home/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "815b3aaf55c7aaa6b330",
+    "url": "/home/static/js/main.0d6ad0ad.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/home/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/home/static/media/resume.9d3da0e2.pdf"
+  }
+]);
